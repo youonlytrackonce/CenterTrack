12b9803585e0f83df7cba12862c501f4b8dc3bdc
youonlytrackonce <f_emre87@hotmail.com>
2023-03-30 21:08:21 +0300

    det and emb test scripts added. Single walking man analysis seems good

--- experiments/infer_sompt22.sh
+++ experiments/infer_sompt22.sh
@@ -1,4 +1,4 @@
 cd src
 # infer
-python demo.py tracking --hungarian --load_model "/home/fatih/phd/CenterTrack/exp/tracking,embedding/sompt22-tracking-embedding/logs_2023-03-26-19-34/model_last.pth" --num_classes 1 --demo /home/fatih/phd/CenterTrack/data/sompt22/images/train/SOMPT22-02/SOMPT22-02.mp4
+python demo.py tracking,embedding --max_age 5 --debug 2 --load_model "/home/fatih/phd/CenterTrack/exp/tracking,embedding/sompt22-tracking-embedding/logs_2023-03-26-19-34/model_last.pth" --num_classes 1 --demo /home/fatih/phd/6186d461-85cac7c4.mp4
 cd ..
